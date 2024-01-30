f68bbbb77f71e507cc9337815569f31bfdfca27a
Sina Siadat <siadat@gmail.com>
2024-01-30 20:30:49 +0000

    update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -206,6 +206,14 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+      },
     },
   },
 
