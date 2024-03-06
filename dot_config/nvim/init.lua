51ebc875cc67c7c621dd75c3d4a0d548690212c7
Sina Siadat <siadat@gmail.com>
2024-03-06 10:31:59 +0000

    chezmoi: update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -109,7 +109,33 @@ require('lazy').setup({
   'tpope/vim-rhubarb',
   {
     'stevearc/oil.nvim',
-    opts = {},
+    opts = {
+      -- copied from https://github.com/stevearc/oil.nvim/blob/18dfd24/README.md
+      columns = {
+        -- "icon", -- sina: icons don't render properly for me
+        -- "permissions",
+        -- "size",
+        -- "mtime",
+      },
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["v"] = "actions.select_vsplit", -- sina: it was ["<C-s>"]
+        ["<C-h>"] = "actions.select_split",
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
+    },
     -- Optional dependencies
     -- dependencies = { "nvim-tree/nvim-web-devicons" },
   },
