5d035f1b21f021cb5826000a0b7c8ad41dcf16ed
Sina Siadat <siadat@gmail.com>
2024-03-06 10:33:03 +0000

    chezmoi: update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -118,22 +118,7 @@ require('lazy').setup({
         -- "mtime",
       },
       keymaps = {
-        ["g?"] = "actions.show_help",
-        ["<CR>"] = "actions.select",
         ["v"] = "actions.select_vsplit", -- sina: it was ["<C-s>"]
-        ["<C-h>"] = "actions.select_split",
-        ["<C-t>"] = "actions.select_tab",
-        ["<C-p>"] = "actions.preview",
-        ["<C-c>"] = "actions.close",
-        ["<C-l>"] = "actions.refresh",
-        ["-"] = "actions.parent",
-        ["_"] = "actions.open_cwd",
-        ["`"] = "actions.cd",
-        ["~"] = "actions.tcd",
-        ["gs"] = "actions.change_sort",
-        ["gx"] = "actions.open_external",
-        ["g."] = "actions.toggle_hidden",
-        ["g\\"] = "actions.toggle_trash",
       },
     },
     -- Optional dependencies
