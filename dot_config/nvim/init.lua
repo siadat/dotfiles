9ca7dbf1f958ce4862db104ac97acdcb2fa6fb0b
Sina Siadat <siadat@gmail.com>
2024-03-21 12:57:58 +0000

    chezmoi: update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -741,7 +741,6 @@ mason_lspconfig.setup_handlers {
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
-vim.cmd.syntax("off")
 vim.keymap.set('n', '<c-f>', '<esc>', { desc = 'sina: escape' })
 vim.keymap.set('i', '<c-f>', '<esc>', { desc = 'sina: escape' })
 vim.keymap.set('v', '<c-f>', '<esc>', { desc = 'sina: escape' })
