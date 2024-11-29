05f2e27c84d7ba7cce44aff0f4f742b6bacda7e4
Sina Siadat <siadat@gmail.com>
2024-11-29 11:39:57 +0100

    update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -95,19 +95,22 @@ require('lazy').setup({
   --   end,
   -- },
   -- 'HiPhish/nvim-ts-rainbow2',
+  -- {
+  --   "ray-x/go.nvim",
+  --   dependencies = {  -- optional packages
+  --     "ray-x/guihua.lua",
+  --     "neovim/nvim-lspconfig",
+  --     "nvim-treesitter/nvim-treesitter",
+  --   },
+  --   config = function()
+  --     require("go").setup()
+  --   end,
+  --   event = {"CmdlineEnter"},
+  --   ft = {"go", 'gomod'},
+  --   build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  -- },
   {
-    "ray-x/go.nvim",
-    dependencies = {  -- optional packages
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("go").setup()
-    end,
-    event = {"CmdlineEnter"},
-    ft = {"go", 'gomod'},
-    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    "fatih/vim-go",
   },
   {
     "scalameta/nvim-metals",
@@ -317,7 +320,7 @@ require('lazy').setup({
   -- 'Olical/conjure',
   {
     "shellpad/shellpad.nvim",
-    dev = vim.fn.hostname() == "personalbox",
+    dev = true, -- vim.fn.hostname() == "personalbox",
     dependencies = { "nvim-telescope/telescope.nvim" },
     config = function(opts)
       require('shellpad').setup(opts)
@@ -734,17 +737,17 @@ require('lazy').setup({
   -- { import = 'custom.plugins' },
 }, { dev = { path = '~/src/nvim-plugins' } })
 
--- https://github.com/ray-x/go.nvim
-require('go').setup()
--- Run gofmt + goimports on save
-local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimports()
-  end,
-  group = format_sync_grp,
-})
+-- -- https://github.com/ray-x/go.nvim
+-- require('go').setup()
+-- -- Run gofmt + goimports on save
+-- local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*.go",
+--   callback = function()
+--    require('go.format').goimports()
+--   end,
+--   group = format_sync_grp,
+-- })
 
 -- require('nvim-treesitter.configs').setup {
 --   rainbow = {
